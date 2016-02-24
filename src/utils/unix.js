d35720a2b0affea5b1a53669195749150113ed75
midniteio <ian@midnite.io>
2016-02-24 14:38:11 -0800

    Adding ambiguous step status color for cucumber-js

--- src/utils/unix.js
+++ src/utils/unix.js
@@ -12,20 +12,20 @@ export function colorize(text, color) {
     red: '\x1B[31m',
     green: '\x1B[32m',
     yellow: '\x1B[33m',
-    cyan: '\x1B[36m'
+    cyan: '\x1B[36m',
+    purple: '\x1B[35m',
   };
   return colors[color] + text + '\x1B[0m';
 };
 
 export const buffer = {
-  data: '',  
+  data: '',
   log: function(text) {
     this.data += text + '\n';
-  },  
+  },
   dump: function() {
     let bufferedData = this.data;
     this.data = '';
     return bufferedData;
   }
 }
-
