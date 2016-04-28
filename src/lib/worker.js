a1ded85badc6561c7753972fbe1864f3adafda91
Guy Kisel <gkisel@riotgames.com>
2016-04-28 11:42:28 -0700

    move worker var initialization to constructor

--- src/lib/worker.js
+++ src/lib/worker.js
@@ -5,55 +5,53 @@ import Promise from 'bluebird';
 export default class Worker {
   constructor(options) {
     this.options = options;
+    this.featureFile = this.options.featureFile;
+    this.featureFileData = path.parse(this.featureFile);
+    this.featureName = this.featureFileData.name;
+    this.scenarioLine = this.options.scenarioLine;
+    this.logFileName = this.featureName + '-line-' + this.scenarioLine + '.json';
+    this.logFile = path.join(this.options.logDir, this.logFileName);
+    this.relativeLogFile = path.relative(process.cwd(), this.logFile);
+    this.cucumberPath = this.options.cucumberPath;
+    this.args = [this.featureFile + ':' + this.scenarioLine, '-f', 'json:' + this.relativeLogFile];
+    this.options.requires.forEach(function(arg) {
+      this.args.push('-r');
+      this.args.push(arg);
+    });
   }
 
   execute() {
     return new Promise(function(resolve) {
-      let featureFile = this.options.featureFile;
-      let featureFileData = path.parse(featureFile);
-      let featureName = featureFileData.name;
-      let scenarioLine = this.options.scenarioLine;
-      let logFileName = featureName + '-line-' + scenarioLine + '.json';
-      let logFile = path.join(this.options.logDir, logFileName);
-      let relativeLogFile = relativeLogFile = path.relative(process.cwd(), logFile);
-      let cucumberPath = this.options.cucumberPath;
-      let args = [featureFile + ':' + scenarioLine, '-f', 'json:' + relativeLogFile];
-
-      this.options.requires.forEach(function(arg) {
-        args.push('-r');
-        args.push(arg);
-      });
+      let startTime = new Date();
 
       // TODO: allow inheriting stdio
       this.child = spawn(
-        cucumberPath,
-        args,
+        this.cucumberPath,
+        this.args,
         {stdio: 'ignore'}
       );
-
-      let startTime = new Date();
 
       this.child.on('exit', function(code) {
         resolve({
           type: 'result',
           exitCode: code,
-          featureFile: featureFile,
-          scenarioLine: scenarioLine,
-          resultFile: logFile,
+          featureFile: this.featureFile,
+          scenarioLine: this.scenarioLine,
+          resultFile: this.logFile,
           duration: new Date() - startTime
         });
-      });
+      }.bind(this));
 
       this.child.on('error', function(err) {
         resolve({
           type: 'result',
           exitCode: 10,
           exception: err,
-          featureFile: featureFile,
-          scenarioLine: scenarioLine,
+          featureFile: this.featureFile,
+          scenarioLine: this.scenarioLine,
           duration: new Date() - startTime
         });
-      });
+      }.bind(this));
     }.bind(this));
   }
 
