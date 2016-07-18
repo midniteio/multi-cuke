27f15825bd0c419a21e2b793db2bc704b14d266a
Guy Kisel <gkisel@riotgames.com>
2016-07-18 16:40:55 -0700

    more gracefully handle undefined exceptions

--- src/lib/parsers/pretty.js
+++ src/lib/parsers/pretty.js
@@ -90,7 +90,10 @@ export default class PrettyParser {
   }
 
   parseException(feature, scenario, exception) {
-    let errorMessage = exception.stack || exception;
+    let errorMessage = 'Unknown error';
+    if (exception) {
+      exception.stack || exception;
+    }
     let featureUri = feature.uri || '';
     buffer.log('Feature: ' + feature.name);
 
