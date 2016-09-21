ee7800cef21cc7cead6f6c33e79d1b1e2e7318ac
Vasiliki Siakka <vsiakka@gmail.com>
2016-09-21 13:42:47 +0300

    When there is a parsing error we should exit with a non 0 exit code

--- test/results/feature-finder.js
+++ test/results/feature-finder.js
@@ -1,150 +1,174 @@
 import path from 'path';
 
 module.exports = {
-  default: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 11
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 14
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample2.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 4
-    },
-    {
-      featureFile: path.join('test','features','sample3.feature'),
-      isScenarioOutline: true,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test','features','sample3.feature'),
-      isScenarioOutline: true,
-      scenarioLine: 8
-    }
-  ],
-  tagged: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 11
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample2.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 4
-    }
-  ],
-  negatedTag: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 14
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample2.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 4
-    },
-    {
-      featureFile: path.join('test','features','sample3.feature'),
-      isScenarioOutline: true,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test','features','sample3.feature'),
-      isScenarioOutline: true,
-      scenarioLine: 8
-    }
-  ],
-  multipleTags: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    }
-  ],
-  mixedTags: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    }
-  ],
-  orOperatorTags: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 11
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample2.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 4
-    }
-  ],
-  featurePath: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 11
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 14
-    }
-  ],
-  multiplePaths: [
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 7
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 11
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 14
-    },
-    {
-      featureFile: path.join('test', 'features', 'sample2.feature'),
-      isScenarioOutline: false,
-      scenarioLine: 4
-    }
-  ]
+  default: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 11
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 14
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample2.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 4
+      },
+      {
+        featureFile: path.join('test','features','sample3.feature'),
+        isScenarioOutline: true,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test','features','sample3.feature'),
+        isScenarioOutline: true,
+        scenarioLine: 8
+      }
+    ],
+    didDetectErrors: false
+  },
+  tagged: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 11
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample2.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 4
+      }
+    ],
+    didDetectErrors: false
+  },
+  negatedTag: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 14
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample2.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 4
+      },
+      {
+        featureFile: path.join('test','features','sample3.feature'),
+        isScenarioOutline: true,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test','features','sample3.feature'),
+        isScenarioOutline: true,
+        scenarioLine: 8
+      }
+    ],
+    didDetectErrors: false
+  },
+  multipleTags: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      }
+    ],
+    didDetectErrors: false
+  },
+  mixedTags: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      }
+    ],
+    didDetectErrors: false
+  },
+  orOperatorTags: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 11
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample2.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 4
+      }
+    ],
+    didDetectErrors: false
+  },
+  featurePath: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 11
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 14
+      }
+    ],
+    didDetectErrors: false
+  },
+  multiplePaths: {
+    scenarios: [
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 7
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 11
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 14
+      },
+      {
+        featureFile: path.join('test', 'features', 'sample2.feature'),
+        isScenarioOutline: false,
+        scenarioLine: 4
+      }
+    ],
+    didDetectErrors: false
+  }
 };
